1aa471a5eb4a3fa9641898061fca4d38b5ed483e
MGK venkatesh <139879845+MGK-VENKATESH@users.noreply.github.com>
2025-01-23 13:26:08 +0530

    Create Clone List with Next and Random.py

---
+++ Hard/Clone List with Next and Random.py
@@ -0,0 +1,77 @@
+Description:
+You are given a special linked list with n nodes where each node has two pointers a next pointer that points to the next node of the singly linked list, and a random pointer that points to the random node of the linked list.
+
+Construct a copy of this linked list. The copy should consist of the same number of new nodes, where each new node has the value corresponding to its original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list, such that it also represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+
+Return the head of the copied linked list.
+
+NOTE : Original linked list should remain unchanged.
+
+Examples:
+
+Input: head = [[1, 3], [3, 3], [5, NULL], [9, 3]] 
+      
+Output: head = [[1, 3], [3, 3], [5, NULL], [9, 3]] 
+Explanation: 
+Node 1 points to Node 2 as its NEXT and Node 3 as its RANDOM.
+Node 2 points to Node 3 as its NEXT and Node 3 as its RANDOM.
+Node 3 points to Node 4 as its NEXT and NULL as its RANDOM.
+Node 4 points to NULL as its NEXT and Node 3 as its RANDOM.
+Input: head = [[1, 3], [2, 1], [3, 5], [4, 3], [5, 2]]
+  
+ 
+Output: head = [[1, 3], [2, 1], [3, 5], [4, 3], [5, 2]]
+Explanation: 
+Node 1 points to Node 2 as its NEXT and Node 3 as its RANDOM.
+Node 2 points to Node 3 as its NEXT and Node 1 as its RANDOM.
+Node 3 points to Node 4 as its NEXT and Node 5 as its RANDOM.
+Node 4 points to Node 5 as its NEXT and Node 3 as its RANDOM.
+Node 5 points to NULL as its NEXT and Node 2 as its RANDOM.
+Input: head = [[7, NULL], [7, NULL]]
+Output: head = [[7, NULL], [7, NULL]]
+Explanation: 
+Node 1 points to Node 2 as its NEXT and NULL as its RANDOM.
+Node 2 points to NULL as its NEXT and NULL as its RANDOM.
+Constraints:
+1 <= n <= 100
+0 <= node->data <= 1000
+
+
+Python3:
+# Link list Node
+# class Node:
+
+#     def __init__(self, x):
+#         self.data = x
+#         self.next = None
+#         self.random = None
+        
+class Solution:
+    def cloneLinkedList(self, head):
+        # code here
+        
+        if not head:
+            return None
+
+        node_map = {}
+
+        dummy = Node(-1)
+        curr_original = head
+        curr_clone = dummy
+
+        while curr_original:
+            new_node = Node(curr_original.data)
+            curr_clone.next = new_node
+            node_map[curr_original] = new_node
+            curr_original = curr_original.next
+            curr_clone = curr_clone.next
+
+        curr_original = head
+        curr_clone = dummy.next
+        while curr_original:
+            if curr_original.random:
+                curr_clone.random = node_map[curr_original.random]
+            curr_original = curr_original.next
+            curr_clone = curr_clone.next
+
+        return dummy.next
