1c187395700b9aea1d87abc409849f5d5ca2b0f1
MGK venkatesh <139879845+MGK-VENKATESH@users.noreply.github.com>
2025-02-07 18:39:17 +0530

    Create Inorder Traversal.py

---
+++ Easy/Inorder Traversal.py
@@ -0,0 +1,42 @@
+Description:
+Given a Binary Tree, your task is to return its In-Order Traversal.
+
+An inorder traversal first visits the left child (including its entire subtree), then visits the node, and finally visits the right child (including its entire subtree).
+
+Follow Up: Try solving this with O(1) auxiliary space.
+
+Examples:
+
+Input: root[] = [1, 2, 3, 4, 5] 
+      
+Output: [4, 2, 5, 1, 3]
+Explanation: The in-order traversal of the given binary tree is [4, 2, 5, 1, 3].
+Input: root[] = [8, 1, 5, N, 7, 10, 6, N, 10, 6]
+      
+Output: [1, 7, 10, 8, 6, 10, 5, 6]
+Explanation: The in-order traversal of the given binary tree is [1, 7, 10, 8, 6, 10, 5, 6].
+Constraints:
+
+1 <= number of nodes <= 105
+0 <= node->data <= 105
+
+Python3:# Definition for a binary tree node
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+
+class Solution:
+    def inOrder(self, root):
+        result = []
+        
+        def inorderTraversal(node):
+            if not node:
+                return
+            inorderTraversal(node.left)   # Visit left subtree
+            result.append(node.data)      # Visit node
+            inorderTraversal(node.right)  # Visit right subtree
+        
+        inorderTraversal(root)
+        return result
