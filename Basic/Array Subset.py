a65744084318083cf938bb508a18a2cd7c0f40e8
MGK venkatesh <139879845+MGK-VENKATESH@users.noreply.github.com>
2025-04-01 12:59:06 +0530

    Create Array Subset.py

---
+++ Basic/Array Subset.py
@@ -0,0 +1,28 @@
+Description:
+Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
+
+Examples:
+
+Input: a[] = [11, 7, 1, 13, 21, 3, 7, 3], b[] = [11, 3, 7, 1, 7]
+Output: true
+Explanation: b[] is a subset of a[]
+Input: a[] = [1, 2, 3, 4, 4, 5, 6], b[] = [1, 2, 4]
+Output: true
+Explanation: b[] is a subset of a[]
+Input: a[] = [10, 5, 2, 23, 19], b[] = [19, 5, 3]
+Output: false
+Explanation: b[] is not a subset of a[]
+Constraints:
+1 <= a.size(), b.size() <= 105
+1 <= a[i], b[j] <= 106
+
+Python3:
+#User function Template for python3
+from collections import Counter
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        # Your code here
+        c = Counter(a)
+        d = Counter(b)
+        return all(c[x] >= d[x] for x in d)
